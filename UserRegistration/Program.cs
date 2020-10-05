3214660a4218bfd4a8b564f5afba13d5f3cf8265
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-05 16:18:41 +0530

    [Sai]Added. First Name Validation

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
     class Program
     {
+
+        public static bool NameValidation(String firstName)
+        {
+            String patternFN = "^[A-Z]{1}[a-z]{2,}$";
+
+            Regex re = new Regex(patternFN);
+            return re.IsMatch(firstName);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Problem!");
+            Console.WriteLine("Welcome to User Registration!");
+
+            // to validate first name
+
+            Console.WriteLine("Enter the First Name");
+            String firstName = Console.ReadLine();
+
+            if (NameValidation(firstName))
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+
         }
     }
 }
