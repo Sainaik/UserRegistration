3c419bc1400467cf204a7f8d8978b307858d119b
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-06 13:13:22 +0530

    [Sai] Merging with UC4-PhoneNumber validation

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,28 +6,12 @@ namespace UserRegistration
     class Program
     {
 
-<<<<<<< HEAD
-        public static bool EmailValidation(String email)
-        {
-
-            //String pattern = @"^([a-z][a-z0-9.+_-]*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?)$";
-
-            //String pattern = @"^[a-z]+[-+*.]?[0-9a-z]*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
-
-            String pattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
-
-            Regex r = new Regex(pattern);
-
-            return r.IsMatch(email);
-
-=======
         public static bool phnoValidation(String phno)
         {
             String patternPN = "^[+][0-9]{1,3}[\\s][0-9]{10}$";
 
             Regex re = new Regex(patternPN);
             return re.IsMatch(phno);
->>>>>>> UC4-PhoneNumberValidation
         }
         static void Main(string[] args)
         {
@@ -35,21 +19,6 @@ namespace UserRegistration
 
             // to validate first name
 
-<<<<<<< HEAD
-            
-            Console.WriteLine("Enter the Email");
-
-            String email = Console.ReadLine();
-
-            if (EmailValidation(email))
-            {
-                Console.WriteLine("Valid email");
-            }
-            else
-            {
-                Console.WriteLine("Invalid email");
-            }
-=======
             Console.WriteLine("Enter the Phone Number");
             String PhoneNumber = Console.ReadLine();
 
@@ -63,7 +32,6 @@ namespace UserRegistration
             }
 
            
->>>>>>> UC4-PhoneNumberValidation
 
             
         }
