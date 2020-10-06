ad7836776692668e4c83c4a3b7e4b1580af6fad2
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-06 13:05:07 +0530

    [Sai]Added. All Usecases of UserRegistration

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,42 +5,64 @@ namespace UserRegistration
 {
     class Program
     {
-
-        public static bool EmailValidation(String email)
+        public static bool PatternValidation(String input, String pattern)
         {
-
-            //String pattern = @"^([a-z][a-z0-9.+_-]*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?)$";
-
-            //String pattern = @"^[a-z]+[-+*.]?[0-9a-z]*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
-
-            String pattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
-
-            Regex r = new Regex(pattern);
-
-            return r.IsMatch(email);
-
+            Regex re = new Regex(pattern);
+            return re.IsMatch(input);
         }
+
+
+        public static String validation(String pattern)
+        {
+            String inputString = Console.ReadLine();
+
+            while (!PatternValidation(inputString, pattern))
+            {
+                Console.WriteLine(inputString+ " is invalid!\nEnter again!!");
+                inputString = Console.ReadLine();
+            }
+            Console.WriteLine(inputString + " is Valid\n");
+
+            return inputString;
+
+        } 
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration!");
 
             // to validate first name
 
+            Console.WriteLine("Enter the First Name(First letter should be capital)");
             
+            String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+            String firstName = validation(firstNamePattern);
+
+            Console.WriteLine("Enter the Last Name(First letter should be capital)");
+           
+            String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+            String lastName = validation(lastNamePattern);
+
+
             Console.WriteLine("Enter the Email");
 
-            String email = Console.ReadLine();
+            String emailPattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
+            String email = validation(emailPattern);
 
-            if (EmailValidation(email))
-            {
-                Console.WriteLine("Valid email");
-            }
-            else
-            {
-                Console.WriteLine("Invalid email");
-            }
 
-            
+            Console.WriteLine("Enter the phone Number");
+
+            String phonePattern = "^[+][0-9]{1,3}[\\s][0-9]{10}$";
+            String phoneNo = validation(phonePattern);
+
+
+            Console.WriteLine("Enter the Password");
+
+            String passwordPattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+            String password = validation(passwordPattern);
+
+
+
         }
     }
 }
